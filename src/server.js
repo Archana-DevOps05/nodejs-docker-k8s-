d53292cd791826a4c30b7d66c25ca069957b03e6
Archana-DevOps05 <archuusingh0509@gmail.com>
2025-02-02 02:54:21 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Friends! This is  node.js code testing  using CI/CD pipeline.');
+    res.send('Hello, Friends! This is  node.js code testing  using CI/CD pipeline, Webhooks.');
 });
 
 app.listen(PORT, () => {
